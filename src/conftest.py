5b48bbdb351f0e19e048fd445bcf3915253114c4
roman <r.godov@malltech.ru>
2023-07-24 13:19:48 +0300

    edit fixture

--- src/conftest.py
+++ src/conftest.py
@@ -11,5 +11,4 @@ class Row():
 
 @pytest.fixture(autouse=True)
 def result():
-    return [Row(id=1, name='Test 1'),
-            Row(id=2, name='Test 2')]
+    return [Row(id=i, name=f'Test {i}') for i in range(1, 61)]
