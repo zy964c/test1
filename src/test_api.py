60279d5e1efd814a36f3755fee977cb946cac6b2
roman <r.godov@malltech.ru>
2023-07-24 13:03:45 +0300

    fix(pytest): get rid of async mock

--- src/test_api.py
+++ src/test_api.py
@@ -1,6 +1,7 @@
-from dataclasses import asdict
-from unittest.mock import AsyncMock, MagicMock
 import pytest
+
+from dataclasses import asdict
+from unittest.mock import MagicMock
 from httpx import AsyncClient
 from main import app
 
@@ -8,14 +9,13 @@ from main import app
 @pytest.mark.asyncio
 class TestEndpoints():
     async def test_raw_sql(self, mocker, result):
-        session_mock = AsyncMock()
-        session_begin_mock = MagicMock()
-        session_mock.__aenter__.return_value = session_begin_mock
+        session_mock = MagicMock()
+        session_mock.__aenter__.return_value = MagicMock()
 
-        async def async_magic():
+        async def async_func():
             return result
 
-        MagicMock.__await__ = lambda x: async_magic().__await__()
+        MagicMock.__await__ = lambda x: async_func().__await__()
         mocker.patch('main.async_sessionmaker').return_value = MagicMock(
             return_value=session_mock)
         async with AsyncClient(app=app, base_url="http://test") as ac:
